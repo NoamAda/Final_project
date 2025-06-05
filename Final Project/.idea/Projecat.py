582ac791c034a8a2342bc71f74d22530e817b2ab
USER <noamada2009@gmail.con>
2025-06-05 20:01:04 +0300

    boa

--- Final Project/.idea/Projecat.py
+++ Final Project/.idea/Projecat.py
@@ -44,7 +44,7 @@ def random_position(board):
             y  = random.randint (0,4)
             posis = (x,y)
             snake_indx.append(posis)
-            random_minus = random.randint(-10,0)
+            random_minus = random.randint(-10,1)
             snake_val.append(random_minus)
             board[x][y] = SNAKE
         for i in range(3):
@@ -52,7 +52,7 @@ def random_position(board):
             y  = random.randint (0,3)
             posup = (x,y)
             latter_indx.append(posup)
-            random_plus = random.randint(0,10)
+            random_plus = random.randint(1,10)
             latter_val.append(random_plus)
             board[x][y] = LATTER
         return board
